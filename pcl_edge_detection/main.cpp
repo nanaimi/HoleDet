7989a648c5b83b6547fbe52b5121f70d7c19f128
hanlonm <hanlonm@student.ethz.ch>
2022-03-23 11:24:05 +0100

    concave hull and projection

--- pcl_edge_detection/main.cpp
+++ pcl_edge_detection/main.cpp
@@ -17,6 +17,9 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/features/boundary.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/surface/concave_hull.h>
+#include <pcl/surface/convex_hull.h>
 
 int main (int argc, char** argv)
 {
@@ -55,6 +58,7 @@ int main (int argc, char** argv)
 
     // extract floor
     pcl::PointCloud<pcl::PointXYZ>::Ptr floor (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr floor_projected (new pcl::PointCloud<pcl::PointXYZ>);
     pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
     // Create the segmentation object
@@ -77,6 +81,25 @@ int main (int argc, char** argv)
     extract.setNegative (true);
     extract.filter (*cloud);
 
+    // Project the model inliers
+    pcl::ProjectInliers<pcl::PointXYZ> proj;
+    proj.setModelType (pcl::SACMODEL_PLANE);
+    // proj.setIndices (inliers);
+    proj.setInputCloud (floor);
+    proj.setModelCoefficients (coefficients);
+    proj.filter (*floor_projected);
+
+    // Create a Concave Hull representation of the projected inliers
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::ConcaveHull<pcl::PointXYZ> chull;
+    chull.setInputCloud (floor_projected);
+    chull.setAlpha (1);
+    chull.reconstruct (*cloud_hull);
+    viewer->addPointCloud(cloud_hull,"hull");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0.0f, 1.0f, 1.0f, "hull");
+    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5,"hull");
+
+
     // boundary
     // fill in the cloud data here
     pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
@@ -96,29 +119,75 @@ int main (int argc, char** argv)
     est.setRadiusSearch (0.5);   // 50cm radius
     est.setSearchMethod (typename pcl::search::KdTree<pcl::PointXYZ>::Ptr (new pcl::search::KdTree<pcl::PointXYZ>));
     est.compute (boundaries);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_boundary(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr floor_boundary(new pcl::PointCloud<pcl::PointXYZ>);
     for(size_t i = 0; i <floor->points.size(); ++i) {
         if(boundaries[i].boundary_point> 0) {
-            cloud_boundary->push_back(floor->points[i]);
+            floor_boundary->push_back(floor->points[i]);
         }
     }
 
+//    viewer->addPointCloud(floor_boundary,"bound");
+//    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0.0f, 1.0f, 1.0f, "bound");
+//    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5,"bound");
 
 
-    viewer->addPointCloud(cloud,"cloud");
-    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0.0f, 1.0f, 0.0f, "cloud");
-    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2,"cloud");
+    // lines
+//    pcl::PointCloud<pcl::PointXYZ>::Ptr line (new pcl::PointCloud<pcl::PointXYZ>);
+//    pcl::ModelCoefficients::Ptr line_coeff(new pcl::ModelCoefficients());
+//    pcl::PointIndices::Ptr      line_inliers(new pcl::PointIndices());
+//    // Segment the largest linear component from the remaining cloud
+//    typename pcl::search::KdTree<pcl::PointXYZ>::Ptr search(new pcl::search::KdTree<pcl::PointXYZ>);
+//    search->setInputCloud(floor_boundary);
+//
+//    pcl::SACSegmentation<pcl::PointXYZ> line_seg;
+//    line_seg.setOptimizeCoefficients(true);
+//    line_seg.setModelType(pcl::SACMODEL_LINE);
+//    line_seg.setMethodType(pcl::SAC_RANSAC);
+//    line_seg.setMaxIterations(10000);
+//    line_seg.setDistanceThreshold(0.2);
+//    line_seg.setInputCloud(floor_boundary);
+//    line_seg.segment(*line_inliers, *line_coeff);
+//    pcl::copyPointCloud<pcl::PointXYZ>(*floor_boundary, *line_inliers, *line);
+//
+//    viewer->addPointCloud(line,"line");
+//    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1.0f, 0.0f, 0.0f, "line");
+//    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5,"line");
+//
+//    for (int i = 0; i < 20; ++i) {
+//        pcl::ExtractIndices<pcl::PointXYZ> line_extract;
+//        line_extract.setInputCloud (floor_boundary);
+//        line_extract.setIndices (line_inliers);
+//        line_extract.setNegative (true);
+//        line_extract.filter (*floor_boundary);
+//
+//        // Run segmentation on outliers
+//        line_seg.setInputCloud (floor_boundary);
+//        line_seg.segment (*line_inliers, *line_coeff);
+//        pcl::copyPointCloud<pcl::PointXYZ>(*floor_boundary, *line_inliers, *line);
+//
+//        auto name = "line_" + std::to_string(i);
+//        viewer->addPointCloud(line,name);
+//        viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1.0f, 0.0f, 0.0f, name);
+//        viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5,name);
+//
+//    }
+
+
+
+//    viewer->addPointCloud(cloud,"cloud");
+//    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0.0f, 1.0f, 0.0f, "cloud");
+//    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2,"cloud");
     viewer->addPointCloud(floor,"floor");
     viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0.0f, 0.2f, 0.8f, "floor");
     viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2,"floor");
 
-    viewer->addPointCloud(trajectory,"trajectory");
-    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1.0f, 0.0f, 1.0f, "trajectory");
-    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5,"trajectory");
+//    viewer->addPointCloud(trajectory,"trajectory");
+//    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1.0f, 0.0f, 1.0f, "trajectory");
+//    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5,"trajectory");
 
-    viewer->addPointCloud(cloud_boundary,"bound");
-    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1.0f, 0.0f, 0.0f, "bound");
-    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5,"bound");
+
+
+
 
     while (!viewer->wasStopped ())
     {
