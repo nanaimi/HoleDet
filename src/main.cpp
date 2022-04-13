27746d105e81324c09a94724723eb25e6adc01a1
hanlonm <hanlonm@student.ethz.ch>
2022-04-13 18:38:23 +0200

    adding relative paths

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,18 @@
 // Created by hanlonm on 06.04.22.
 //
 #include "HoleDetector.h"
+namespace fs = boost::filesystem;
 
 int main (int argc, char** argv)
 {
-    HoleDetector holeDetector ("/home/hanlonm/HoleDet/Data/hololens.pcd",
-                               "/home/hanlonm/HoleDet/Data/floorplan.jpg" );
+
+    std::string data_path = "/../data/";
+    fs::path current = fs::current_path();
+    auto path = current.string() + data_path;
+
+    HoleDetector holeDetector (path + "hololens.pcd",
+                               path + "floorplan.jpg" );
     holeDetector.detectHoles();
-    holeDetector.getFloorplanCloud(true, "/home/hanlonm/HoleDet/Data/floorplan.pcd");
+    holeDetector.getFloorplanCloud(true, path + "floorplan.pcd");
     holeDetector.visualize();
 }
